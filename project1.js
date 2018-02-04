766b3ab632eaeb200ba764af811356ad6b9e8113
Rosana Sincere <rosanasincere@gmail.com>
2018-02-04 10:51:08 -0600

    Update project1.js

--- project1.js
+++ project1.js
@@ -1,10 +1,17 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script>
 
-$('#contact-form').on('submit',
+  /*$('#contact-form').on('submit',
   function() {
     console.log('The form was submitted.');
     var name = $('#name').val();
     $(this).remove();
     $('body').append('<h1>Thank You" + name + '</h1>');
   }
-);
+);*/
+   $(document).ready(function(){
+    $("button").click(function(){
+        $("input").hide();
+       $('body').append('<h1>Thank You" + name + '</h1>');
+    });
+</script>
