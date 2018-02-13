5901e5f3c104dc6858b3c6758b30c6ac327b430d
Rosana Sincere <rosanasincere@gmail.com>
2018-02-13 17:23:17 -0600

    js

--- project1.js
+++ project1.js
@@ -4,10 +4,10 @@
     $('#myform')[0].reset();
 };
 
-/*$(document).ready(function(){
+$(document).ready(function(){
     $("button").click(function(){
         $("ol").hide();
        $('body').append('<h1> Thank You ' + name + '</h1>');
+       $('body').append('<h1> Confirm your email for a special treat! </h1>');
     });
  });
-*/
